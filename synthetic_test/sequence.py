0fe120b78a77eaf7a72caf3913b1543d8f940578
dumpling <yaroslav.mykoliv28@gmail.com>
2020-01-27 20:57:55 +0100

    Add parameter parser for synthetic.py and add support for rectangular resolution

--- synthetic_test/sequence.py
+++ synthetic_test/sequence.py
@@ -1,9 +1,10 @@
 import numpy as np
 import random
+import math
 
 class SequenceConfig:
     def __init__(self, resolution, fps, duration):
-        self.res = resolution
+        self.res = list(reversed(resolution))
         self.fps = fps
         self.duration = duration
         self.frame_num = fps * duration
@@ -13,10 +14,12 @@ class MovingEdgeFrameIterator:
     def __init__(self, sequence_config):
         self.conf = sequence_config
         self.index = 1
-        self.data = np.zeros((self.conf.res, self.conf.res))
+        self.data = np.zeros(self.conf.res)
         self.start_frame = self.data.copy()
-        self.move_ratio = int(self.conf.frame_num / self.conf.res)
-        self.row = np.zeros(self.conf.res)
+        self.move_ratio = self.conf.res[1] / self.conf.frame_num 
+        self.position = 0
+        self.row = np.zeros(self.conf.res[1])
+        print(self.data.shape)
         self.col_index = 0
 
     def __iter__(self):
@@ -25,10 +28,13 @@ class MovingEdgeFrameIterator:
     def __next__(self):
         if self.index == self.conf.frame_num:
             raise StopIteration
-        if self.index % self.move_ratio == 0:
-            for i in range(0,self.conf.res):
-                self.data[i][self.col_index] = 255
-            self.col_index=(self.col_index+1)%self.conf.res
+        self.position += self.move_ratio
+        if self.index == 1:
+            self.data = self.start_frame.copy()
+        else:
+            for i in range(0,self.conf.res[0]):
+                self.data[i][:int(self.position)] = 255
+            self.col_index=(self.col_index+1)%self.conf.res[1]
         self.index += 1
         return self.data
 
@@ -38,7 +44,7 @@ class SingleColorFrameIterator:
     def __init__(self, value, sequence_config):
         self.conf = sequence_config
         self.index = 1
-        self.frame = np.full((self.conf.res, self.conf.res), value)
+        self.frame = np.full((self.conf.res), value)
         self.start_frame = self.frame
 
     def __iter__(self):
@@ -70,7 +76,7 @@ class RandomPixelFrameIterator:
 
     def rand_frame(self):
         return np.random.randint(self.value_range[0], high=self.value_range[1],
-               size=(self.conf.res, self.conf.res))
+               size=self.conf.res)
 
 class CheckersFrameIterator:
     """Produces consequent frames of checkerboard pattern with each iteration"""
@@ -94,7 +100,7 @@ class CheckersFrameIterator:
         return result
 
     def frame(self, even):
-        mat = np.zeros((self.conf.res, self.conf.res))
+        mat = np.zeros(self.conf.res)
         if even:
             for row in mat[0::2]:
                 row[0::2] = 255
@@ -113,7 +119,7 @@ class RandomBinaryChangeFrameIterator:
     def __init__(self, rate, sequence_config):
         self.conf = sequence_config
         self.index = 1
-        self.start_frame = np.zeros((self.conf.res, self.conf.res))
+        self.start_frame = np.zeros(self.conf.res)
         self.val = self.start_frame.copy()
         self.events = {} 
         self.rate = rate
@@ -131,12 +137,12 @@ class RandomBinaryChangeFrameIterator:
     def set_from_events(self, events):
         self.events = events.copy()
         for event in events:
-            i = int(event / self.conf.res)
-            j = event - i*self.conf.res
+            i = int(event / self.conf.res[1])
+            j = event - i*self.conf.res[1]
             self.val[i][j] = RandomBinaryChangeFrameIterator.invert(self.val[i][j])
 
     def change_rand(self, rate):
-        res_sq = self.conf.res * self.conf.res
+        res_sq = self.conf.res[0] * self.conf.res[1]
         num = int(res_sq * rate)
         population = range(0, res_sq)
         self.set_from_events(random.sample(population, num))
--- synthetic_test/synthetic.py
+++ synthetic_test/synthetic.py
@@ -13,38 +13,52 @@ import shutil
 import random
 import events
 import sequence as seq
+import argparse
 
-resolution = 64
-fps = 30
-duration = 5
-np.random.seed(seed=0)
-out_name = 'default_name'
-method = sys.argv[1]
-format = sys.argv[-1].split('.')[-1]
-out_name = '.'.join(sys.argv[-1].split('.')[:-1]) 
-if len(sys.argv) == 6:
-    resolution = int(sys.argv[2])
-    fps = int(sys.argv[3])
-    duration = int(sys.argv[4])
+# Define script interface
+parser = argparse.ArgumentParser(description='Perform binary search of event \
+        rate performance threshold')
+parser.add_argument('sequence', help='type of sequences generated', 
+                    choices=['moving_edge', 'random_pixel', 'single_color', 
+                             'checkers', 'rate_random_flip'])
+parser.add_argument('-r', '--res', dest='res', action='store', default=[64, 64],
+                    nargs=2, type=int,
+                    help='Resolution of the generated sequences: y x')
+parser.add_argument('-f', '--fps', dest='fps', action='store', default=30,
+                    help='Framerate of the generated sequences')
+parser.add_argument('-d', '--duration', dest='duration', action='store',
+                    type=int, default=5,
+                    help='Duration of the generated sequences')
+parser.add_argument('--rate', type=float, default=0.5)
+parser.add_argument('--value', type=int, default=0)
+parser.add_argument('--range', type=int, default=[0, 255], nargs=2)
+parser.add_argument('out', help='output file')
+args = parser.parse_args()
+print(args)
 
-sequence_config = seq.SequenceConfig(resolution, fps, duration)
+sequence_config = seq.SequenceConfig(args.res, args.fps, args.duration)
 frame_iterators = {
         'moving_edge': seq.MovingEdgeFrameIterator(sequence_config), 
-        'random_pixel': seq.RandomPixelFrameIterator((0,2), sequence_config),
-        'single_color': seq.SingleColorFrameIterator(255, sequence_config), 
-        'checkers': seq.CheckersFrameIterator(sequence_config)
+        'random_pixel': seq.RandomPixelFrameIterator(
+                        (args.range[0],args.range[1]), sequence_config),
+        'single_color': seq.SingleColorFrameIterator(args.value, sequence_config), 
+        'checkers': seq.CheckersFrameIterator(sequence_config),
+        'rate_random_flip': seq.RandomBinaryChangeFrameIterator(
+                            args.rate, sequence_config)
 }
 format_iterators = {
         'aer': events.AERIterator,
         'caer': events.CAERIterator
 }
 
-frame_iterator = frame_iterators[method]
+out_name = '.'.join(args.out.split('.')[:-1]) 
+format = args.out.split('.')[-1]
+frame_iterator = frame_iterators[args.sequence]
 file_name = f"{out_name}.{format}"
 
 os.makedirs(os.path.dirname(file_name), exist_ok=True)
 out = open(file_name, "wb+")
-events.create_raw_file(out, resolution, fps, duration)
+events.create_raw_file(out, args.res, args.fps, args.duration)
 events.save_frame(out, frame_iterator.start_frame)
 if format == 'raw':
     os.mkdir(out_name)
