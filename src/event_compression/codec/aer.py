0163418ea6d644a7512b79c82e568642c4f7ae02
dumpling <yaroslav.mykoliv28@gmail.com>
2020-04-06 19:42:46 +0200

    Add and pass AER codec tests

--- src/event_compression/codec/aer.py
+++ src/event_compression/codec/aer.py
@@ -1,62 +1,120 @@
 """Define AER-like codecs"""
 import numpy as np
 import struct
-from . import codec
+from .util import codec
+import functools
+import itertools
+import pdb
 
 @codec(name="aer")
 class AER:
     """
     Define encoder and decoder for AER format.
+
+    The first frame is saved as is: matrix of numbers.
+    Then the sequence of events is produced.
     
     AER format represents each event as:
-        i (4 bytes), j (4 bytes), t (float), value (1 byte), polarity (1 byte)
-    
-    If value >= 127, two events are created at the same timestamp (127 and
-    residual), so that each value can be stored with one byte.
+        value (1 byte), polarity (1 byte) i (4 bytes), j (4 bytes), t (4 bytes)
 
     Polarity -- sign of value:
         For positive values: polarity > 0.
         For negative values: polarity = 0.
     """
 
+    event_size = 14
+    buffer = bytearray(event_size)
+
     @classmethod
     def encoder(cls, frames) -> bytearray:
+        """
+        Yield binary representation of events from sequence of video frames
+        for single frame at a time.
+        """
+        #pdb.set_trace()
         frame_it = iter(frames)
-        prev = next(frame_it)
-        overflow = bytearray()
+        prev = next(frame_it).copy()
+
+        #Encode resolution and number of frames
+        yield struct.pack('>3I', prev.shape[0], prev.shape[1], len(frames))
+
+        #Encode first frame
+        yield prev.tobytes()
 
         for t, frame in enumerate(frame_it):
-            diff = np.subtract(frame, prev)
+            diff = np.subtract(frame, prev, dtype='int16')
             result = bytearray()
-            for i, row in enumerate(diff):
-                for j, value in enumerate(row):
-                    value = int(value)
-                    if value == 0:
-                        continue
-                    result += i.to_bytes(4, byteorder='big', signed=False)
-                    result += j.to_bytes(4, byteorder='big', signed=False)
-                    result += t.to_bytes(4, byteorder='big', signed=False) 
+            for (i, j), value in np.ndenumerate(diff):
+                value = int(value)
+                if value == 0:
+                    continue
 
-                    sign = 0 if value < 0 else 255
-                    if abs(value) >= 127:
-                        result += int(127).to_bytes(1, byteorder='big', signed=False)
-                        overflow = int(abs(value) - 127)
-                        result += i.to_bytes(4, byteorder='big', signed=False)
-                        result += j.to_bytes(4, byteorder='big', signed=False)
-                        result += t.to_bytes(4, byteorder='big', signed=False)
-                        result += int(sign & overflow).to_bytes(1,
-                                  byteorder='big', signed=False)
-                    else:
-                        result += int(sign & abs(value)).to_bytes(1,
-                                byteorder='big', signed=False)
+                polarity = 0 if value < 0 else 1
+                cls._append_event(result, i, j, t, abs(value), polarity)
+        
+            #Mark end of events for timestamp t
+            #result += int(0).to_bytes(1, byteorder='big', signed=False)
+
             prev = frame.copy()
             if len(result) != 0:
                 yield result
     
     @classmethod
-    def decoder(cls, events):
-        pass
+    def decoder(cls, data) -> np.ndarray:
+        data_it = iter(data)
 
+        header = struct.unpack('>3I', cls.take_next(data_it, 12))
+        res = (header[0], header[1])
+        frame_num = header[2]
+
+        frame = cls._decode_first_frame(data_it, res) 
+        yield frame
+
+        t = 0
+        prev_t = 0
+        while True:
+            try:
+                buffer = cls.take_next(data_it, 14)
+            except:
+                break
+
+            i,j,t,value,polarity = struct.unpack('>3I2B', buffer)
+            
+            if t > prev_t:
+                for _ in range(t-prev_t):
+                    yield frame
+                prev_t = t
+            
+            value = value if polarity == 1 else -value
+            frame[i,j] += value
+        yield frame
+
+        for i in range(frame_num-t-2):
+            yield frame
+
+    
+    @staticmethod
+    def take_next(it, n):
+        result = bytearray()
+        for _ in range(n):
+            result += next(it).to_bytes(1, byteorder='big', signed=False)
+        return result
+    
+    @staticmethod
+    def _append_event(result, x, y, t, value, polarity):
+        result += x.to_bytes(4, byteorder='big', signed=False)
+        result += y.to_bytes(4, byteorder='big', signed=False)
+        result += t.to_bytes(4, byteorder='big', signed=False)
+        result += value.to_bytes(1, byteorder='big', signed=False)
+        result += polarity.to_bytes(1, byteorder='big', signed=False)
+
+    @staticmethod
+    def _decode_first_frame(data, res) -> np.ndarray:
+        result = np.zeros(res, dtype='uint8')
+        for index, _ in np.ndenumerate(result):
+            result[index] = next(data)
+        return result
+        
 @codec(name="aer_binary")
 class AERBinary:
     """
--- src/tests/codec/test_aer.py
+++ src/tests/codec/test_aer.py
@@ -1,11 +1,157 @@
 import pytest
+import src.event_compression.synthetic as synthetic
+from src.event_compression.synthetic.sequence import *
 import src.event_compression.codec as codec
 from src.event_compression.codec import aer as aer
+import itertools
+import functools
+import numpy as np
+import pdb
+import struct
+import copy
 
-@pytest.fixture
-def smtp_connection():
-    import smtplib
-    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
+def simple_seq():
+    """
+    Produce sequence with events (x, y, t, v, p):
+        (1, 1, 1, 255, +)
+        (1, 1, 3, 248, -)
+        (0, 0, 4, 255, +)
+        (1, 1, 4, 7,   -)
+    """
+    return [
+    np.array([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ], dtype='uint8'),
+    np.array([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ], dtype='uint8'),
+    np.array([
+        [0, 0, 0],
+        [0, 255, 0],
+        [0, 0, 0]
+    ], dtype='uint8'),
+    np.array([
+        [0, 0, 0],
+        [0, 255, 0],
+        [0, 0, 0]
+    ], dtype='uint8'),
+    np.array([
+        [0, 0, 0],
+        [0, 7, 0],
+        [0, 0, 0]
+    ], dtype='uint8'),
+    np.array([
+        [255, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ], dtype='uint8')]
+
+def reduce(encoder):
+    return functools.reduce(lambda x, y: x+y, encoder, bytearray())
+
+def common_test(cls=None):
+    """
+    Define common tests for all aer codecs.
+
+    @common_test(cls=aer_class_to_be_tested)
+    class TestClass:
+        pass
+    """
+    def decorate(_cls):
+        _cls.cls = cls
+
+        def test_invertability(instance, sequence, correct):
+            code = reduce(instance.cls.encoder(sequence))
+            decoder = instance.cls.decoder(code)
+            frames = [x.copy() for x in decoder]
+            for i, f in enumerate(frames):
+                np.testing.assert_equal(f, correct[i])
+        
+        #Add all invertability tests
+        for name, sequence in synthetic.sequences().items():
+            config = Config((3,3),10,1,value=127,rate=0.1)
+            np.random.seed(seed=0)
+            correct_seq = [x.copy() for x in sequence(config)]
+            seq = copy.deepcopy(correct_seq)
+            def caller(seq, correct_seq):
+                return lambda inst: test_invertability(inst,seq,correct_seq)
+            setattr(_cls, f'test_invertability_{name}', caller(seq, correct_seq))
+        fn = lambda inst: test_invertability(inst, simple_seq(), simple_seq())
+        setattr(_cls, f'test_{cls.__name__}_invertability_simple', fn)
+        
+        return _cls
+    return decorate
 
 def test_init():
 	assert codec.codecs()
+
+@common_test(aer.AER)
+class TestAER:
+    def test_encoder(self):
+        sequence = simple_seq()
+        code = reduce(self.cls.encoder(sequence))
+        correct_code = struct.pack('>3I 9B 3I2B 3I2B 3I2B 3I2B',
+            3,3,6,
+            0,0,0,0,0,0,0,0,0,
+            1,1,1,255,1,
+            1,1,3,248,0,
+            0,0,4,255,1,
+            1,1,4,7,0)
+        assert code == correct_code
+
+    def test_encode_moving_edge(self):
+        """
+        [[0 0 0]
+        [0 0 0]
+        [0 0 0]]
+
+        [[255   0   0]
+        [255   0   0]
+        [255   0   0]]
+
+        [[255 255   0]
+        [255 255   0]
+        [255 255   0]]
+
+        [[255 255   0]
+        [255 255   0]
+        [255 255   0]]
+        """
+        #pdb.set_trace()
+        seq = MovingEdge(Config((3,3), 2, 2))
+        code = reduce(self.cls.encoder(seq))
+        correct_code = struct.pack('>3I 9B 3I2B 3I2B 3I2B 3I2B 3I2B 3I2B',
+            3,3,4,
+            0,0,0,0,0,0,0,0,0,
+            
+            0,0,0,255,1,
+            1,0,0,255,1,
+            2,0,0,255,1,
+
+            0,1,1,255,1,
+            1,1,1,255,1,
+            2,1,1,255,1)
+        
+        assert code == correct_code
+    
+    def test_encode_single_color(self):
+        seq = SingleColor(Config((3,3),4,1,value=127))
+        code = reduce(self.cls.encoder(seq))
+        correct_code = struct.pack('>3I 9B',
+            3,3,4,
+            127,127,127,127,127,127,127,127,127)
+        assert code == correct_code
+
+    def test_decode_single_color(self):
+        seq = [x for x in SingleColor(Config((3,3),4,1,value=127))]
+        code = struct.pack('>3I 9B',
+            3,3,4,
+            127,127,127,127,127,127,127,127,127)
+        frames = [x.copy() for x in self.cls.decoder(code)]
+        for i, frame in enumerate(frames):
+            np.testing.assert_equal(frame, seq[i])
+
